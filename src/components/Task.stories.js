7b2743d53513b06a18f7ec8bb13d441cb0a7b6d4
duchangkim <duchang.dev@gmail.com>
2022-10-11 11:17:02 +0900

    add: Add long title case

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -35,3 +35,12 @@ Archived.args = {
     state: 'TASK_ARCHIVED',
   },
 };
+
+const longTitleString = '모든 국민은 거주·이전의 자유를 가진다. 대통령은 법률이 정하는 바에 의하여 훈장 기타의 영전을 수여한다. 국회의원은 국가이익을 우선하여 양심에 따라 직무를 행한다. 국가는 농수산물의 수급균형과 유통구조의 개선에 노력하여 가격안정을 도모함으로써 농·어민의 이익을 보호한다. 국채를 모집하거나 예산외에 국가의 부담이 될 계약을 체결하려 할 때에는 정부는 미리 국회의 의결을 얻어야 한다.';
+export const LongTitle = Template.bind({});
+LongTitle.args = {
+  task: {
+    ...Default.args.task,
+    title: longTitleString,
+  }
+}
