8ef612642b28a1c42f1cce435040e29014cb8306
duchangkim <duchang.dev@gmail.com>
2022-10-11 10:45:03 +0900

    add: Constuct a screen

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Taskbox 타이틀이 정상적으로 렌더 되었는가', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const taskBoxTitle = screen.getByText(/Taskbox/i);
+  expect(taskBoxTitle).toBeInTheDocument();
 });
